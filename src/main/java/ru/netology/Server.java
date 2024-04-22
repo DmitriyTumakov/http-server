0b0703ceb97f4c0b067a4b1071707acb6c3f575f
Dmitriy Tumakov <dmit.tumakov@gmail.com>
2024-04-22 17:36:43 +0300

    Handlers added

--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -7,9 +7,12 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.AttributedCharacterIterator;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -17,6 +20,7 @@ import java.util.concurrent.Executors;
 public class Server {
     public ExecutorService threadPool = Executors.newFixedThreadPool(64);
     private List<String> validPaths = new ArrayList<>();
+    private Map<String, Map<String, Handler>> methodMap = new HashMap<>();
 
     public Server(List<String > validPaths) {
         this.validPaths = validPaths;
@@ -28,6 +32,22 @@ public class Server {
         } catch (IOException | ExecutionException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public void addHandler(String method, String path, Handler handler) {
+        for (var methodKey : methodMap.keySet()) {
+            if (method.equals(methodKey)) {
+                for (var titleKey : methodMap.get(methodKey).keySet()) {
+                    if (path.equals(titleKey)) {
+                        methodMap.get(methodKey).replace(titleKey, handler);
+                        return;
+                    }
+                }
+                methodMap.get(methodKey).put(path, handler);
+                return;
+            }
+        }
+        methodMap.put(method, (Map<String, Handler>) new HashMap<String, Handler>().put(path, handler));
     }
 
     private void serverAccept(ServerSocket serverSocket) throws ExecutionException, InterruptedException {
@@ -51,6 +71,9 @@ public class Server {
                             }
 
                             final var path = parts[1];
+                            final var method = parts[0];
+                            Request request = new Request(method, path);
+
                             if (!validPaths.contains(path)) {
                                 out.write((
                                         "HTTP/1.1 404 Not Found\r\n" +
@@ -85,6 +108,19 @@ public class Server {
                             }
 
                             final var length = Files.size(filePath);
+                            if (!methodMap.isEmpty()) {
+                                for (var methodKey : methodMap.keySet()) {
+                                    if (method.equals(methodKey)) {
+                                        for (var titleKey : methodMap.get(methodKey).keySet()) {
+                                            if (path.equals(titleKey)) {
+                                                methodMap.get(methodKey).get(titleKey).handle(request, out);
+                                                out.flush();
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
                             out.write((
                                     "HTTP/1.1 200 OK\r\n" +
                                             "Content-Type: " + mimeType + "\r\n" +
