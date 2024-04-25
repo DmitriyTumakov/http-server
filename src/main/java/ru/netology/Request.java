7e61321be66d384db575c51231c35cf69f507adb
Dmitriy Tumakov <dmit.tumakov@gmail.com>
2024-04-25 04:26:09 +0300

    Query added

--- src/main/java/ru/netology/Request.java
+++ src/main/java/ru/netology/Request.java
@@ -1,18 +1,22 @@
 package ru.netology;
 
+import org.apache.hc.core5.net.URLEncodedUtils;
+
+import java.nio.charset.StandardCharsets;
+
 public class Request {
     private String method;
-    private String title;
+    private String path;
     private String body;
 
-    public Request(String method, String title) {
+    public Request(String method, String path) {
         this.method = method;
-        this.title = title;
+        this.path = path;
     }
 
     public Request(String method, String title, String body) {
         this.method = method;
-        this.title = title;
+        this.path = title;
         this.body = body;
     }
 
@@ -20,11 +24,32 @@ public class Request {
         return method;
     }
 
-    public String getTitle() {
-        return title;
+    public String getPath() {
+        return path;
     }
 
     public String getBody() {
         return body;
+    }
+
+    public String getQueryParam(String title) {
+        final var pathArray = path.split("\\?");
+        final var queryParamList = URLEncodedUtils.parse(pathArray[1], StandardCharsets.UTF_8);
+        for (int i = 0; i < queryParamList.size(); i++) {
+            if (queryParamList.get(i).getName().equals(title)) {
+                return queryParamList.get(i).getName() + queryParamList.get(i).getValue();
+            }
+        }
+        return null;
+    }
+
+    public String getQueryParams() {
+        final var pathArray = path.split("\\?");
+        final var queryParamList = URLEncodedUtils.parse(pathArray[1], StandardCharsets.UTF_8);
+        final var stringBuilder = new StringBuilder();
+        for (int i = 0; i < queryParamList.size(); i++) {
+            stringBuilder.append(queryParamList.get(i));
+        }
+        return stringBuilder.toString();
     }
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,26 +1,19 @@
 package ru.netology;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.text.AttributedCharacterIterator;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
     public ExecutorService threadPool = Executors.newFixedThreadPool(64);
-    private List<String> validPaths = new ArrayList<>();
-    private Map<String, Map<String, Handler>> methodMap = new HashMap<>();
+    private final List<String> validPaths;
+    private final Map<String, Map<String, Handler>> methodMap = new HashMap<>();
 
     public Server(List<String > validPaths) {
         this.validPaths = validPaths;
@@ -57,24 +50,56 @@ public class Server {
                     while (true) {
                         try (
                                 final var socket = serverSocket.accept();
-                                final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                                final var in = new BufferedInputStream(socket.getInputStream());
                                 final var out = new BufferedOutputStream(socket.getOutputStream())
                         ) {
                             // read only request line for simplicity
                             // must be in form GET /path HTTP/1.1
-                            final var requestLine = in.readLine();
-                            final var parts = requestLine.split(" ");
 
-                            if (parts.length != 3) {
-                                // just close socket
-                                continue;
+                            final var limit = 4096;
+                            in.mark(limit);
+                            final var buffer = new byte[limit];
+                            final var read = in.read(buffer);
+
+                            final var requestLineDelimiter = new byte[]{'\r', '\n'};
+                            final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+                            final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+
+                            final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+                            final var headersStart = requestLineEnd + requestLineDelimiter.length;
+                            final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+                            in.reset();
+                            in.skip(headersStart);
+
+                            final var headersBytes = in.readNBytes(headersEnd - headersStart);
+                            final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+
+                            final var pathWithQuery = requestLine[1];
+                            final var method = requestLine[0];
+                            final var pathArray = pathWithQuery.split("\\?");
+                            final var path = pathArray[0];
+
+                            String body = null;
+                            if (!method.equals("GET")) {
+                                in.skip(headersDelimiter.length);
+                                final var contentLength = extractHeader(headers, "Content-Length");
+                                if (contentLength.isPresent()) {
+                                    final var length = Integer.parseInt(contentLength.get());
+                                    final var bodyBytes = in.readNBytes(length);
+                                    body = new String(bodyBytes);
+                                }
                             }
 
-                            final var path = parts[1];
-                            final var method = parts[0];
-                            Request request = new Request(method, path);
+                            Request request = null;
+                            if (!method.equals("GET")) {
+                                request = new Request(method, pathWithQuery, body);
+                            } else {
+                                request = new Request(method, pathWithQuery);
+                            }
 
-                            if (!validPaths.contains(path)) {
+                            System.out.println(request.getQueryParams());
+
+                            if (!validPaths.contains(pathWithQuery)) {
                                 out.write((
                                         "HTTP/1.1 404 Not Found\r\n" +
                                                 "Content-Length: 0\r\n" +
@@ -85,7 +110,7 @@ public class Server {
                                 continue;
                             }
 
-                            final var filePath = Path.of(".", "public", path);
+                            final var filePath = Path.of(".", "public", pathWithQuery);
                             final var mimeType = Files.probeContentType(filePath);
 
                             // special case for classic
@@ -139,5 +164,26 @@ public class Server {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    private static Optional<String> extractHeader(List<String> headers, String header) {
+        return headers.stream()
+                .filter(o -> o.startsWith(header))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
     }
 }
